ed59d7868ff3aabe7c8dc052e45642b82c8ef655
Alex Sander <alexsander01@hotmail.com.br>
2022-10-05 19:37:17 -0300

    feat: add axios

---
+++ src/components/categories/categories.component.tsx
@@ -0,0 +1,34 @@
+import './categories.styles.css'
+import { useEffect, useState } from 'react'
+
+import Category from '../../types/category.types'
+import env from '../../config/env.config'
+import axios from 'axios'
+
+const Categories = () => {
+  const [categories, setCategories] = useState<Category[]>([])
+
+  console.log({ categories })
+
+  const featchCategories = async () => {
+    try {
+      const { data } = await axios.get(`${env.apiUrl}/api/category`)
+
+      setCategories(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    featchCategories()
+  }, [])
+  return (
+    <div className="categories-container">
+        <div className="categories-content">
+
+        </div>
+    </div>
+  )
+}
+
+export default Categories
--- src/components/pages/home/home.page.tsx
+++ src/components/pages/home/home.page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Categories from '../../categories/categories.component'
 import Header from '../../header/header.components'
 
 const HomePage = () => {
   return (
     <div>
         <Header/>
+
+        <Categories/>
     </div>
   )
 }
