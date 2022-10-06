97d96698c729578d988d94891b77d287590b1b3d
Alex Sander <alexsander01@hotmail.com.br>
2022-10-06 19:21:46 -0300

    fix: update category item background image

--- src/components/category/category-item-component.tsx
+++ src/components/category/category-item-component.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from 'react'
 import Category from '../../types/category.types'
 
-import './category-item;styles.css'
+import './category-item.styles.css'
 
 interface CategoryItemProps {
     category: Category
@@ -9,7 +9,7 @@ interface CategoryItemProps {
 
 const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
   return (
-        <div className="category-item-container" style={{ backgroundImage: category.imageUrl }}>
+        <div className="category-item-container" style={{ backgroundImage: `url( '${category.imageUrl}')` }}>
             <div className="category-name">
              <p>{category.displayName}</p>
              <p>Explorar</p>
