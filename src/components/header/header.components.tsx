a2a4b65613f6f1ba910e75e56fe99f2d16277e15
Alex Sander <alexsander01@hotmail.com.br>
2022-10-06 20:25:28 -0300

    refactor: update

--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 // components
-import './header.styles.css'
+// import './header.styles.css'
 
 // icons
 import { BsCart3 } from 'react-icons/bs'
+import { HeaderContainer, HeaderItem, HeaderItems, HeaderTile } from './header.styles'
+
+// styles
+
 const Header = () => {
   return (
-    <div className="header-container">
-        <h2 className="header-title">Sander Shop</h2>
-        <div className="header-items-container">
-            <div className="header-items">
-                <div className="header-item">Explorar</div>
-                <div className="header-item">Login</div>
-                <div className="header-item">Login</div>
-                <div className="header-item"><BsCart3 size={25}/></div>
-            </div>
-        </div>
-    </div>
+    <HeaderContainer>
+        <HeaderTile>Sander Shop</HeaderTile>
+        <HeaderItems>
+
+                <HeaderItem>Explorar</HeaderItem>
+                <HeaderItem>Login</HeaderItem>
+                <HeaderItem>Login</HeaderItem>
+                <HeaderItem><BsCart3 size={25}/>5</HeaderItem>
+
+        </HeaderItems>
+    </HeaderContainer>
   )
 }
 
