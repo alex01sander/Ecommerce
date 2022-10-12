ae0911ff765ecd443adba8990271580c7c99e0fe
Alex Sander <alexsander01@hotmail.com.br>
2022-10-12 18:23:42 -0300

    feat: add React hooh form validação

--- src/components/custom-input/custom-input.component.tsx
+++ src/components/custom-input/custom-input.component.tsx
@@ -1,16 +1,19 @@
 import './custom-input.styles.ts'
 
-import { FunctionComponent, InputHTMLAttributes } from 'react'
+import React, { FunctionComponent, InputHTMLAttributes } from 'react'
 import { CustomInputContainer } from './custom-input.styles'
 
 interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement>{
     hasError?: boolean
 }
 
-const CustomInput:FunctionComponent<CustomInputProps> = ({ hasError, ...rest }) => {
+const CustomInput:FunctionComponent<CustomInputProps> = React.forwardRef((props, ref) => {
   return (
-        <CustomInputContainer hasError={hasError} {...rest}/>
+              <CustomInputContainer {...props} ref={ref as any}/>
   )
 }
+)
+
+CustomInput.displayName = 'CustomInput'
 
 export default CustomInput
--- src/components/pages/login/login.page.tsx
+++ src/components/pages/login/login.page.tsx
@@ -6,11 +6,20 @@ import Header from '../../header/header.components'
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from './login.styles'
 
 // ICONS
+import { useForm } from 'react-hook-form'
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
 import CustomInput from '../../custom-input/custom-input.component'
 
 const LoginPage = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log({ data })
+  }
+
+  console.log(errors)
+
   return (
     <>
         <Header/>
@@ -24,13 +33,17 @@ const LoginPage = () => {
                 <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
 
                 <LoginInputContainer>
-                    <CustomInput placeholder='Digite o seu e-mail'/>
+                    <p>E-mail</p>
+                    <CustomInput hasError={!!errors?.email}
+                    placeholder='Digite o seu e-mail' {...register('email', { required: true })}/>
                 </LoginInputContainer>
                 <LoginInputContainer>
-                    <CustomInput placeholder='Digite sua senha '/>
+                    <p>Senha</p>
+                    <CustomInput hasError={!!errors?.password}
+                    placeholder='Digite sua senha ' {...register('password', { required: true })}/>
                 </LoginInputContainer>
 
-                <CustomButton startIcon={<FiLogIn size={18}/>}>Entrar</CustomButton>
+                <CustomButton startIcon={<FiLogIn size={18}/>} onClick={() => handleSubmit(handleSubmitPress)()}>Entrar</CustomButton>
             </LoginContent>
 
         </LoginContainer>
