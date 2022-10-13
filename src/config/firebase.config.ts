8ba6a427e7118c708af89f0558a9fbcd0d9fa413
Alex Sander <alexsander01@hotmail.com.br>
2022-10-13 20:36:32 -0300

    feat: add atualização do firestore

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,6 @@
 
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from '@firebase/app'
+import { getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBkXyocb6gHV7T2MK9qBJGMHu-1rUGMT_4',
