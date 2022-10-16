74a366b5c9926e564ee388f8687164e63b76be35
Alex Sander <alexsander01@hotmail.com.br>
2022-10-16 15:38:17 -0300

    feat: add Sign In out

--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom'
 // icons
 import { BsCart3 } from 'react-icons/bs'
 import { HeaderContainer, HeaderItem, HeaderItems, HeaderTile } from './header.styles'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../config/firebase.config'
 
 // styles
 
@@ -27,6 +29,7 @@ const Header = () => {
                 <HeaderItem>Explorar</HeaderItem>
                 <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
                 <HeaderItem onClick={handleSignUoClick}>Criar Conta</HeaderItem>
+                <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
                 <HeaderItem><BsCart3 size={25}/>5</HeaderItem>
 
         </HeaderItems>
