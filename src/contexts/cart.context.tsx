135ad93f51edb9ef7ad6ba1f3c2f35936691b236
Alex Sander <alexsander01@hotmail.com.br>
2022-10-25 18:36:30 -0300

    feat: add local storage no carrinho

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,4 +1,4 @@
-import { createContext, FunctionComponent, ReactNode, useMemo, useState } from 'react'
+import { createContext, FunctionComponent, ReactNode, useEffect, useMemo, useState } from 'react'
 import CartProduct from '../types/cart.types'
 import Product from '../types/product.types'
 
@@ -33,6 +33,18 @@ export const CartContext = createContext<ICartContext>({
 const CartContextProvider: FunctionComponent<Children> = ({ children }) => {
   const [isVisible, setIsVisible] = useState(false)
   const [products, setProducts] = useState<CartProduct[]>([])
+
+  useEffect(() => {
+    const productsFromLocalStorage = JSON.parse(
+      localStorage.getItem('cartProducts')!
+    )
+
+    setProducts(productsFromLocalStorage)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('cartProducts', JSON.stringify(products))
+  }, [products])
 
   const productsTotalPrice = useMemo(() => {
     return products.reduce((acc, currentProoduct) => {
